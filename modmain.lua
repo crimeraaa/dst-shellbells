56b163e438a417075f8f4f7e406f62229a32743d
crimeraaa <124979548+crimeraaa@users.noreply.github.com>
2023-03-17 11:52:43 +0800

    Add files via upload

--- modmain.lua
+++ modmain.lua
@@ -5,11 +5,11 @@ _G.setfenv(1, _G)
 local prettyname = "[009] Shell Bell Music:"
 print(prettyname.." Begin compiling all songs!")
 
-mysongs = {}
+mysongs = {}    -- instead of a million insividual global variables, index this new global table instead
 local TimeVal = MOD_ENV.require("functions/to_timeval")
 local songlist = MOD_ENV.require("songlist")
 for i, data in pairs(songlist) do
-    local transpose = data.transpose or 0 
+    local transpose = data.transpose or 0 -- data.transpose can actually just be omitted if you don't need it.
     _G.mysongs[data.title] = TimeVal(data.title, data.notes, transpose) 
     -- immediately create a key the global table mysongs for your newly converted song 
 end 
@@ -18,49 +18,26 @@ print(prettyname.." Done compiling all songs.")
 
 --[[
 
-TO SPAWN IN YOUR SONG:
+FORMAT:
     c_shellsfromtable(mysongs.yoursongname, startpos, placementfn, spacing_mult)
-        -- when calling your song variable, always start with "mysongs." (including the dot!)
-        -- "yoursongname" is the data.title you declared in the songfile itself.
-        -- startpos and placementfn can be set to nil, it will spawn the song at your cursor.
-        -- spacing mult will multiply all the shells' distances. 
 
-    For example:  
-    c_shellsfromtable(mysongs.goodday_vox, nil, nil, 4.0)
-        -- simply typing c_shellsfromtable(goodday_vox) will only use the distances defined there, which is *very* short.
-
-    Using the functions I've included below, you can have something like the below code.
-    (use Console++ or similar mods to allow multiple lines!):
-
-    local here = Coords(90, 0, -326)
-    c_shellsfromtable(mysongs.whiplash_bass, here, NW, 4)
-    here.z = -325
-    c_shellsfromtable(mysongs.whiplash_melody, here, NW, 4)
-    here.z = -327
-    c_shellsfromtable(mysongs.whiplash_comp, here, NW, 4)
-
-    Or how about this?
-
-    local here = PlayerPos()
-    c_shellsfromtable(mysongs.blackbird_gtr1, here, SE, 6)
-    c_shellsfromtable(mysongs.blackbird_gtr2, here, SE, 6)
-    here.z = here.z - 2
-    c_shellsfromtable(mysongs.blackbird_vox, here, SE, 6)
-
+EXAMPLE:
+    local here = Coords(90, 0, -300)
+    local dir = NW
+    local mult = 5
+    c_shellsfromtable(mysongs.whiplash_melody, here, dir, mult)
 ]]
-
--- EASE OF USE FUNCTIONS FROM HERE ON (ADMIN PERMISSIONS ONLY)
 
 if TheNet and ((TheNet:GetIsServer() and TheNet:GetServerIsDedicated()) or
 (TheNet:GetIsClient() and not TheNet:GetIsServerAdmin())) then
-    print(prettyname.."You do not have admin permissions on this server. Returning.")
+    print(prettyname.." You do not have admin permissions on this server. Returning.")
     return
 end
 
 -- STARTPOS FUNCTIONS
--- You can call the below functions for the startpos paramater.
-function PlayerPos(player_num) -- if no input for player_num, it will default to you.
-    local player = AllPlayers[player_num] or ThePlayer
+
+function PlayerPos(num) 
+    local player = AllPlayers[num] or ThePlayer
     local coords = {}
     coords.x, coords.y, coords.z = player.Transform:GetWorldPosition()
     return coords
@@ -69,8 +46,8 @@ end
 function Coords(x,y,z) -- quickly create a table of coordinates with x y z as the keys!
     local coords = {}
     y = y or 0
-    if (x == nil or z == nil) then -- if no input for x or z, 
-        return nil  -- startpos will default to the coordinates under your cursor.
+    if (x == nil or z == nil) then 
+        return nil 
     else
         coords.x, coords.y, coords.z = x,y,z 
         return coords
@@ -78,12 +55,6 @@ function Coords(x,y,z) -- quickly create a table of coordinates with x y z as th
 end
 
 -- PLACEMENTFN DIRECTION FUNCTIONS
-
---[[
-You can call the below functions as variables for the placementfn parameter.
-    by default, you spawn a straight line of shells in the specified compass direction.
-        for example:  c_shellsfromtable(goodday, PlayerPosition(), NE, 4.0)
-]]
 
 N = function(pos, mult)
     return Vector3(pos.x - 1 * mult, 0, pos.z + 1 * mult)
@@ -116,60 +87,69 @@ SE = function(pos, mult)
     return Vector3(pos.x + 1 * mult, 0, pos.z)
 end
 
--- EASE OF USE FUNCTIONS
+-- EASE OF USE FUNCTIONS 
+
+local function GetShard()
+    local shard = "this shard"
+    if TheWorld:HasTag("forest") then
+        shard = "Surface"
+    elseif TheWorld:HasTag("caves") then
+        shard = "Caves"
+    elseif TheWorld:HasTag("island") then
+        shard = "Shipwrecked"
+    elseif TheWorld:HasTag("volcano") then
+        shard = "Volcano"
+    end
+    return shard
+end
+
+local function GetShells(radius, remove)
+    local ents = {}
+    local range = ""
+
+    if radius == nil then
+        range = GetShard()
+        ents = Ents
+    else
+        assert(type(radius) == "number", "Please input a NUMBER value!")
+        range = "a "..radius.." unit radius"
+
+        local x,y,z = ThePlayer.Transform:GetWorldPosition()
+        ents = TheSim:FindEntities(x,y,z, radius)
+    end
+
+    local count = 0
+    local shell = "singingshell_octave"
+    for k,v in pairs(ents) do
+        if (v.prefab == shell.."3") or (v.prefab == shell.."4") or (v.prefab == shell.."5") then
+            if remove == true then
+                v:Remove()
+            end
+            count = count + 1
+        end
+    end
+
+    local msg = string.format("We have [%d] shell bells in %s.", count, range)
+
+    if remove == true then
+        msg = string.gsub(msg, "have", "removed")
+    end
+
+    print(msg)
+    return
+end
+
+function ShellsRemove(radius)     -- this command is very robust. be careful with how you use it!        
+    GetShells(radius, true)     -- 2nd argument must be a boolean, see GetShells declaration above.
+end
 
 function ShellsCount(radius)
-    local ents = {}
-    if radius ~= nil then
-        local x,y,z = ThePlayer.Transform:GetWorldPosition()
-        ents = TheSim:FindEntities(x,y,z, radius)
-    else
-        ents = Ents
-    end
-    local count = 0
-
-    for k,v in pairs(ents) do
-        if (v.prefab == "singingshell_octave3") or 
-        (v.prefab == "singingshell_octave4") or 
-        (v.prefab == "singingshell_octave5") then
-            count = count + 1
-        end
-    end
-
-    if radius ~= nil then
-        print(string.format("We have [%d] shell bells in a %d unit radius.", count, radius))
-    else
-        print(string.format("We have [%d] shell bells from this Shard.", count))
-    end
+    GetShells(radius)
 end
 
-function ShellsRemove(radius)     -- this command is very robust. be careful with how you use it!
-    local ents = {}
-    if radius ~= nil then
-        local x,y,z = ThePlayer.Transform:GetWorldPosition()
-        ents = TheSim:FindEntities(x,y,z, radius)
-    else
-        ents = Ents
-    end
-
-    local count = 0
-    for k,v in pairs(ents) do
-        if (v.prefab == "singingshell_octave3") or 
-        (v.prefab == "singingshell_octave4") or 
-        (v.prefab == "singingshell_octave5") then
-            v:Remove()
-            count = count + 1
-        end
-    end
-
-    if radius ~= nil then
-        print(string.format("We successfully removed [%d] shell bells in a %d unit radius.", count, radius))
-    else
-        print(string.format("We successfully removed [%d] shell bells from this Shard.", count))
-    end
-end
 
 -- FOR MY TESTING WORLD ONLY, COMMENT OUT ON PUBLICATION
+
 HomeTime = function()
     c_gonext("sewing_mannequin")
     return
--- scripts/songlist.lua
+++ scripts/songlist.lua
@@ -1,45 +1,12 @@
-local songlist = {} -- primary table we'll return
-
+local songlist = {}
 local directory = {} -- this is where you'll define your folders.
-local dir = directory -- i want the table variable "directory" to be clear and concise, but use "dir" when creating subtables.
-
---[[
-
-Put your song's address (without .lua) inside the dir["My Songs"] table! 
-    Make sure you're INSIDE THE SCOPE OF THE BRACKETS {}. Follow the examples below for help.
-    Always enclose your song's address in quotation marks, and after each entry end it with a comma (",").
-
-]]
+local dir = directory -- use dir["My Folder's Path"] when declaring new subtables.
 
 dir["My Songs"] = 
 {
     "template",
     "jazzlick",
 }
-
---[[
-
-If you have another folder within "scripts/songs", define it as:
-
-dir["My Folder"] =
-{
-    "song1",
-    "song2",
-}
-
-If your folder is INSIDE of a folder within "scripts/songs", like inside of "scripts/songs/My Songs", 
-then define it like this:
-
-dir["My Songs/My Subfolder"] =
-
-{
-    "song1",
-    "song2",
-}
-
-If you have any number of subfolders, as long as they're located under "scripts/songs" it will work.
-
-]]
 
 -- EXAMPLE SONGS (from the "#-# Examples" folders)
 
@@ -84,18 +51,25 @@ dir["7-4 Examples"] =
 
 for name, contents in pairs(directory) do
     for _, file in pairs(contents) do
-        local path = name.."/"..file
+
+        file = string.gsub(file, ".lua", "") -- error proofing just so folks get less headaches
+
+        path = name.."/"..file
+
+        if string.find(name, "/", -1) or string.find(name, "\\", -1) then   
+            path = name..file -- accomodate if user appended any kind of slash at end of folder path
+        end
+
         table.insert(songlist, path)
     end
 end
 
 
 for i,path in pairs(songlist) do
-    local address = "songs/"..path -- if your address is wrong or it's outside the My Songs folder,
+    local address = "songs/"..path 
     local invalid = address.." is missing a valid"
-
     --print("Checking "..address..".") 
-    local songfile = require(address) -- you WILL crash.
+    local songfile = require(address) 
     if songfile.title == nil or not (type(songfile.title) == "string") then
         print(invalid.." data.title value!") -- prevent valid songfiles with invalid values from flat out crashing us.
     elseif songfile.notes == nil or not (type(songfile.notes) == "table") then
