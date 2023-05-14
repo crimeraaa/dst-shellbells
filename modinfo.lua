06bfdf95a4fc0d0ce3c818e2afc7277c0e0aeff5
crimeraaa <124979548+crimeraaa@users.noreply.github.com>
2023-05-14 21:42:09 +0800

    Update modinfo version

--- modinfo.lua
+++ modinfo.lua
@@ -9,7 +9,7 @@ This mod intended to be used as a template for you to create your own shell bell
 Please look in the mod files and create your own song file inside the "scripts/My Songs" folder.
 If you'd like, you can refer to the example song files for help. Also see the modmain for functions you can use that may be helpful.]]
 
-version = "1.1.0"
+version = "1.1.1"
 api_version = 10 
 
 dst_compatible = true
